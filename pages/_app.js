067afc753769d8d01e1dc7f49bf6af69080f7270
Riju Pramanik <pramanikriju@gmail.com>
2021-06-27 22:04:48 +0530

    minor style fixes

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,11 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon)} />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Staatliches"
+          href="https://fonts.googleapis.com/css?family=Karla"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Proza+Libre:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
+          rel="stylesheet"
         />
         <link
           rel="stylesheet"
