1c1606bfc2e64ca26a2d79cfcf39134701f6dcf2
Riju Pramanik <pramanikriju@gmail.com>
2021-06-27 22:20:55 +0530

    font switched to Inter

--- pages/_app.js
+++ pages/_app.js
@@ -17,7 +17,7 @@ const MyApp = ({ Component, pageProps }) => {
         <link rel="shortcut icon" href={getStrapiMedia(global.favicon)} />
         <link
           rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Karla"
+          href="https://fonts.googleapis.com/css?family=Inter"
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Proza+Libre:ital,wght@0,400;0,500;0,600;0,700;0,800;1,400;1,500;1,600;1,700;1,800&display=swap"
